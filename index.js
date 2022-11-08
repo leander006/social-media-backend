92fc1379740b340f0771787b92fa15c186883ab0
leander006 <leanderdsilva06@gmail.com>
2022-11-08 19:05:06 +0530

    fixing mongoDb errors

--- index.js
+++ index.js
@@ -52,7 +52,11 @@ app.use("/api/post", postRoute);
 app.use("/api/comment", commentRoute);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then(console.log("Connected to mongodb"))
   .catch((err) => {
     console.log("invalid", err);
