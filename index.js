2ee88243c60147cd467ec0644e3c5757fd76de1f
leander006 <leanderdsilva06@gmail.com>
2022-11-08 13:51:30 +0530

    updated cors error

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: "*",
-    methods: "GET,POST,PUT,DELETE",
+    origin: ["*"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
+    origin: true,
   })
 );
 
