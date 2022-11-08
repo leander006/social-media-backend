f48208442b3d878e682a3961e52d72b4e008ed61
leander006 <leanderdsilva06@gmail.com>
2022-11-08 10:10:28 +0530

    updated index.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,149 +1,146 @@
 const User = require("../model/User");
-const bcrypt = require('bcrypt');
-const generateToken = require('../config/authToken')
-const asyncHandler = require('express-async-handler')
+const bcrypt = require("bcrypt");
+const generateToken = require("../config/authToken");
+const asyncHandler = require("express-async-handler");
 const passport = require("passport");
 const sendEmail = require("../utils/sendEmail");
 const crypto = require("crypto");
 const Token = require("../model/Token");
 // register //
 
-const registration = asyncHandler(async(req,res) =>{
+const registration = asyncHandler(async (req, res) => {
+  const { username, email, password, name } = req.body;
 
-    const {username,email,password,name} = req.body;
+  if (!username || !email || !password || !name) {
+    return res.status(401).json({ error: "Please enter all  field" });
+  }
 
-    if(!username || !email || !password || !name)
-    {
-        return res.status(401).json({error:"Please enter all  field"});
-    } 
+  const userExist = await User.findOne({ username: username });
+  const emailExist = await User.findOne({ email: email });
 
-    const userExist = await User.findOne({username:username});
-    const emailExist = await User.findOne({email:email})
-
-    try {
-         if(userExist )
-        {
-            return res.status(400).send({message:"Username Exists"});
-        }
-        else  if(emailExist)
-        {
-            return res.status(400).send({message:"Email Exists"});
-        }
-  
-          const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(req.body.password,salt);
-        const newUser = new User({
-            username:username,
-            email:email,
-            password:hashPassword,
-            name:name
-        })
-
-        const user = await newUser.save();
-
-        const token =await new Token({
-            userId:user._id,
-            token:crypto.randomBytes(32).toString("hex"),
-        }).save();
-        const url =`${process.env.BASE_URL}users/${user._id}/verify/${token.token}`;
-        await sendEmail(user.email,"Verify email",url);
-
-        res.status(200).send({message:"An email send for verification"});
-    } catch (error) {
-        res.status(500).send({message:error.message});
-           
+  try {
+    if (userExist) {
+      return res.status(400).send({ message: "Username Exists" });
+    } else if (emailExist) {
+      return res.status(400).send({ message: "Email Exists" });
     }
 
-})
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: username,
+      email: email,
+      password: hashPassword,
+      name: name,
+    });
+
+    const user = await newUser.save();
+
+    const token = await new Token({
+      userId: user._id,
+      token: crypto.randomBytes(32).toString("hex"),
+    }).save();
+    const url = `${process.env.BASE_URL}users/${user._id}/verify/${token.token}`;
+    await sendEmail(user.email, "Verify email", url);
+
+    res.status(200).send({ message: "An email send for verification" });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
 
 // login //
 
-const login =asyncHandler(async(req,res)=>{
-    const {username} = req.body;
-    console.log(req.body.password );
-    try {
-        if(!username || !req.body.password )
-        {
-            return res.status(402).send({message:"Please all field"})
-        }
-        const user = await User.findOne({username});
-        console.log(user);
-        if(!user)
-        {
-            return res.status(400).send({message:"User does not exits!"});
-        }
-        const validate = await bcrypt.compare(req.body.password,user.password)
-        if(!validate)
-        { 
-            return res.status(402).send({message:"Invalid password"})
-        }
-           //Send confirmation email//
-           let tokens = await Token.findOne({userId:user._id})
-        if(user.isVerified === "false")
-        { 
-          if(tokens){
-              return res.status(401).send({message:"Email not verified check your gmail!"});
-            }
-            else{
-                const token =await new Token({
-                    userId:user._id,
-                    token:crypto.randomBytes(32).toString("hex"),
-                }).save();
-                const url =`${process.env.BASE_URL}users/${user._id}/verify/${token.token}`;
-                await sendEmail(user.email,"Verify email",url);
-                return res.status(402).send({message:"Email send check your gmail!"});
-            }
-        }
-        if(user.isVerified === "true"){
-            const { password, ...others } = user._doc;
-            const token=generateToken(user.id);
-            res.cookie("token",token,{ expires: new Date(Date.now() + 25892000000),
-             secure:process.env.NODE_ENV === "production"?true:false,
-             httpOnly:process.env.NODE_ENV === "production"?true:false,})
-             res.cookie("data",JSON.stringify(others),{ expires: new Date(Date.now() + 25892000000),
-                 secure:process.env.NODE_ENV === "production"?true:false,
-                 httpOnly:process.env.NODE_ENV === "production"?true:false,})
-             .status(200).json({others})
-       }
-    
-    // //------------------------------------------//
-    
-  
-    } catch (error) {
-        res.status(501).send({message:error.message})
-       console.log(error);
+const login = asyncHandler(async (req, res) => {
+  const { username } = req.body;
+  console.log(req.body.password);
+  try {
+    if (!username || !req.body.password) {
+      return res.status(402).send({ message: "Please all field" });
     }
-    
-})
- 
-const logout = ((req,res)=>{
-      req.logout();
-      console.log("logout");
-      res.redirect("http://localhost:3000");
-})
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).send({ message: "User does not exits!" });
+    }
+    const validate = await bcrypt.compare(req.body.password, user.password);
+    if (!validate) {
+      return res.status(402).send({ message: "Invalid password" });
+    }
+    //Send confirmation email//
+    let tokens = await Token.findOne({ userId: user._id });
+    if (user.isVerified === "false") {
+      if (tokens) {
+        return res
+          .status(401)
+          .send({ message: "Email not verified check your gmail!" });
+      } else {
+        const token = await new Token({
+          userId: user._id,
+          token: crypto.randomBytes(32).toString("hex"),
+        }).save();
+        const url = `${process.env.BASE_URL}users/${user._id}/verify/${token.token}`;
+        await sendEmail(user.email, "Verify email", url);
+        return res
+          .status(402)
+          .send({ message: "Email send check your gmail!" });
+      }
+    }
+    if (user.isVerified === "true") {
+      const { password, ...others } = user._doc;
+      const token = generateToken(user.id);
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 25892000000),
+        secure: process.env.NODE_ENV === "production" ? true : false,
+        httpOnly: process.env.NODE_ENV === "production" ? true : false,
+      });
+      res
+        .cookie("data", JSON.stringify(others), {
+          expires: new Date(Date.now() + 25892000000),
+          secure: process.env.NODE_ENV === "production" ? true : false,
+          httpOnly: process.env.NODE_ENV === "production" ? true : false,
+        })
+        .status(200)
+        .json({ others });
+    }
 
-const callback =passport.authenticate('google', { failureRedirect: '/login' })
+    // //------------------------------------------//
+  } catch (error) {
+    res.status(501).send({ message: error.message });
+    console.log(error);
+  }
+});
 
+const logout = (req, res) => {
+  req.logout();
+  console.log("logout");
+  res.redirect("http://localhost:3000");
+};
 
-const google = passport.authenticate("google", { scope: [ 'email' ] })
+const callback = passport.authenticate("google", { failureRedirect: "/login" });
 
-const callFunction =((req, res) =>{
-    // Successful authentication, redirect home.
-    const token=generateToken(req.user._id);
-    res.cookie("token",token,{ expires: new Date(Date.now() + 25892000000),
-     secure:process.env.NODE_ENV === "production"?true:false,
-     httpOnly:process.env.NODE_ENV === "production"?true:false,})
-     res.cookie("data",JSON.stringify(req.user),{ expires: new Date(Date.now() + 25892000000),
-         secure:process.env.NODE_ENV === "production"?true:false,
-         httpOnly:process.env.NODE_ENV === "production"?true:false,})
-    //  .status(200).json({others})
-    res.redirect("http://localhost:3000/home");
-  })
+const google = passport.authenticate("google", { scope: ["email"] });
+
+const callFunction = (req, res) => {
+  // Successful authentication, redirect home.
+  const token = generateToken(req.user._id);
+  res.cookie("token", token, {
+    expires: new Date(Date.now() + 25892000000),
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    httpOnly: process.env.NODE_ENV === "production" ? true : false,
+  });
+  res.cookie("data", JSON.stringify(req.user), {
+    expires: new Date(Date.now() + 25892000000),
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    httpOnly: process.env.NODE_ENV === "production" ? true : false,
+  });
+  //  .status(200).json({others})
+  res.redirect("http://localhost:3000/home");
+};
 module.exports = {
-	registration,
-    login,
-    logout,
-    google,
-    callback,
-    callFunction
+  registration,
+  login,
+  logout,
+  google,
+  callback,
+  callFunction,
 };
--- server/index.js
+++ server/index.js
@@ -1,23 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+const chatRoute = require("./routes/chatRoute");
+const authRoute = require("./routes/authRoute");
+const userRoute = require("./routes/userRoute");
+const messageRoute = require("./routes/messageRoute");
+const postRoute = require("./routes/postRoute");
+const commentRoute = require("./routes/commentRoute");
 
-const chatRoute = require("./routes/chatRoute")
-const authRoute = require('./routes/authRoute');
-const userRoute = require('./routes/userRoute');
-const messageRoute = require('./routes/messageRoute');
-const postRoute = require("./routes/postRoute")
-const commentRoute = require("./routes/commentRoute")
+const cors = require("cors");
+const cookieSession = require("cookie-session");
 
-const cors = require('cors')
-const cookieSession = require('cookie-session');
-
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const app = express();
 const passportSetup = require("./utils/passport");
-const passport = require("passport")
-
+const passport = require("passport");
 
 // for multer upload //
 // app.use("/uploadProfile",express.static("uploadProfile"))
@@ -25,99 +23,101 @@ const passport = require("passport")
 
 dotenv.config();
 
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(
-      cookieSession({ name:"session", keys:["leander"], maxAge: 60 * 60 * 100 })
+  cookieSession({ name: "session", keys: ["leander"], maxAge: 60 * 60 * 100 })
 );
-
-app.use(cors({
-      origin:"http://localhost:3000",
-      methods: "GET,POST,PUT,DELETE",
-      credentials:true
-}));  
 
 app.use(passport.initialize());
 app.use(passport.session());
-    
 
+app.use("/api/auth", authRoute);
 
-app.use("/api/auth",authRoute);
+app.use("/api/user", userRoute);
 
-app.use("/api/user",userRoute);
+app.use("/api/chat", chatRoute);
 
-app.use("/api/chat",chatRoute);
+app.use("/api/message", messageRoute);
 
-app.use("/api/message",messageRoute);
+app.use("/api/post", postRoute);
 
-app.use("/api/post",postRoute)
+app.use("/api/comment", commentRoute);
 
-app.use("/api/comment",commentRoute)
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(console.log("Connected to mongodb"))
+  .catch((err) => {
+    console.log("invalid", err);
+  });
 
+// app.use(express.static(path.join(__dirname, "/client/build")));
 
-mongoose.connect(process.env.MONGO_URI).then(console.log("Connected to mongodb")).catch((err)=>{console.log("invalid",err)})
-
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+// });
+app.get("/", (req, res) => {
+  res.send("Welcome to server of Talkology");
 });
-
-const server = app.listen(process.env.PORT || 3001,()=>{
-      console.log(`Backend runnig on port ${process.env.PORT}`);
-})
-
-
+const server = app.listen(process.env.PORT || 3001, () => {
+  console.log(`Backend runnig on port ${process.env.PORT}`);
+});
 
 //Socket //
 
-const io = require('socket.io')(server,{
-      pingTimeout:60000,
-      cors:{
-            origin:"http://localhost:3000"
-      }
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
 
-io.on("connection",(socket) =>{
-      // console.log("Connect to socket",socket.id);
+io.on("connection", (socket) => {
+  // console.log("Connect to socket",socket.id);
 
-      socket.on("setup",(userData) =>{
-            socket.join(userData._id);
-            socket.emit("connected");
-      })
-      socket.on("join room",(room) =>{
-            socket.join(room)
-            // console.log("User joined room",room);
-      })
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+  socket.on("join room", (room) => {
+    socket.join(room);
+    // console.log("User joined room",room);
+  });
 
-      socket.on("typing", (room) => socket.in(room).emit("typing"));
-      socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-      socket.on("new message",(messageRecieved) =>{
-            var chat = messageRecieved.chat
-            if(!chat.users) return console.log("Users are undefined");
-            chat.users.forEach((user) =>{
-                  if(user._id == messageRecieved.sender._id)return
-            socket.in(user._id).emit("message recieved",messageRecieved)
-      })
-      })
+  socket.on("new message", (messageRecieved) => {
+    var chat = messageRecieved.chat;
+    if (!chat.users) return console.log("Users are undefined");
+    chat.users.forEach((user) => {
+      if (user._id == messageRecieved.sender._id) return;
+      socket.in(user._id).emit("message recieved", messageRecieved);
+    });
+  });
 
-      socket.on("new message delete",(message) =>{
-            var chat = message.chat
-            if(!chat.users) return console.log("Users are undefined");
-            chat.users.forEach((user) =>{
-                  if(user._id == message.sender._id)return
-            socket.in(user._id).emit("message deleted",message)
-      })
-      })
-      socket.off("setup",() =>{
-            console.log("User Disconnected");
-            socket.leave(userData._id)
-      })
-
-})
-
+  socket.on("new message delete", (message) => {
+    var chat = message.chat;
+    if (!chat.users) return console.log("Users are undefined");
+    chat.users.forEach((user) => {
+      if (user._id == message.sender._id) return;
+      socket.in(user._id).emit("message deleted", message);
+    });
+  });
+  socket.off("setup", () => {
+    console.log("User Disconnected");
+    socket.leave(userData._id);
+  });
+});
 
 // end //
